87578c1d938255fe0f90a153e984387a7d9b910b
chrisramsey <cramsey@helpsocial.com>
2016-02-22 21:47:37 -0600

    No error when duplicate, just ignore

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 'use strict';
 
 const fs = require('fs');
-
 const files = fs.readdirSync('./src');
 files.map(x => x.replace('.js', ''))
      .forEach(x => {
-         if (Array.prototype[x]) {
-             throw new Error(`Array prototype ${x} already exists.`);
-         }
-
-         Array.prototype[x] = require(`./src/${x}`);
+         Array.prototype[x] = Array.prototype[x] || require(`./src/${x}`);
      });
